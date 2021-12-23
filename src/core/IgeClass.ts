d247b32d832e4aa9eaddee595d7869874f2143e0
Damien Vesper <ldamienvesper@gmail.com>
2021-12-23 14:23:27 -0600

    ???

--- src/core/IgeClass.ts
+++ src/core/IgeClass.ts
@@ -1,5 +1,16 @@
 class IgeClass {
-    
+    classID: string;
+
+    components: any[];
+
+    constructor (class, options) {}
+
+    addComponent = (Component: IgeClass, options: Object): any => {
+        const newComponent = new Component(this, options);
+        this.components.push(newComponent);
+
+        return this;
+    };
 }
 
 export {
